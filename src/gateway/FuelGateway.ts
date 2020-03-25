b63e370175c3e68a69a048ccb7f59d9e0e871772
David Mihal <david@dmihal.com>
2020-03-25 15:54:18 -0300

    Create test wallet

--- src/gateway/FuelGateway.ts
+++ src/gateway/FuelGateway.ts
@@ -1,7 +1,7 @@
-export default class FuelGateway {
-  private wallets: { [name: string]: any } = {};
+import { Gateway } from '@burner-wallet/core/gateways'
 
-  constructor() {}
+export default class FuelGateway extends Gateway {
+  private wallets: { [name: string]: any } = {};
 
   isAvailable() {
     return true;
@@ -15,7 +15,7 @@ export default class FuelGateway {
     return null;
   }
 
-  send(network: string, payload: any) {
+  async send(network: string, payload: any) {
     const _network = network.substr(0, network.indexOf('-'));
     const wallet = this.getWallet(_network);
 
