3821c4e7dabe169fbad0e0f1f0c2934f4b9fcccc
David Mihal <david@dmihal.com>
2020-03-28 22:46:03 -0300

    Hide bar after use

--- src/plugin/FaucetBar.tsx
+++ src/plugin/FaucetBar.tsx
@@ -4,21 +4,36 @@ import styled from 'styled-components';
 import FuelPlugin from './FuelPlugin';
 import FuelAsset from '../asset';
 
+const REQUEST_TIMEOUT = 12 * 60 * 60 * 1000;
+
 const Container = styled.div`
   margin: 4px 0;
-  background: #EEEEEE;
   display: flex;
+  justify-content: center;
 `;
 
 const ENSPanel: React.FC<PluginElementContext> = ({ defaultAccount, plugin, assets, BurnerComponents }) => {
+  const lastRequested = parseInt(localStorage.getItem('fuel-faucet-date') || '0');
+
   const [loading, setLoading] = useState(false);
+  const [hidden, setHidden] = useState(!lastRequested || Date.now() - lastRequested > REQUEST_TIMEOUT);
   const _plugin = plugin as FuelPlugin;
 
   const request = async (asset: Asset) => {
     setLoading(true);
-    await (asset as FuelAsset).faucet(defaultAccount);
+    try {
+      await (asset as FuelAsset).faucet(defaultAccount);
+      localStorage.setItem('fuel-faucet-date', Date.now().toString());
+      setHidden(true);
+    } catch (e) {
+      console.warn(e);
+    }
     setLoading(false);
   };
+
+  if (hidden) {
+    return null;
+  }
 
   const { Button } = BurnerComponents;
   const faucetAssets = assets.filter((asset: Asset) => _plugin.faucets.indexOf(asset.id) !== -1);
